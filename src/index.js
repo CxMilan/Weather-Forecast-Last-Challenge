e5dbbe59b4f35286ca49e0649c324364804e5ff8
Cate Milan <catherine.milanetti@gmail.com>
2024-04-11 00:06:31 -0400

    Clean up the javascript

--- src/index.js
+++ src/index.js
@@ -100,5 +100,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Montreal");
-getForecast("Montreal");
 displayForecast();
